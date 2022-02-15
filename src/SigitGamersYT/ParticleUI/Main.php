9afa29d37cc7a821d26ceb0d2d0ecf25c58c6a80
SigitGamers <74124100+SigitGamers@users.noreply.github.com>
2022-02-15 08:18:03 +0700

    Create Main.php

---
+++ src/SigitGamersYT/ParticleUI/Main.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace SigitGamersYT\ParticleUI;
+
+use pocketmine\Server;
+use pocketmine\player\Player;
+
+use pocketmine\utils\Color;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+
+class Main extends PluginBase implements Listener {
+
+}
