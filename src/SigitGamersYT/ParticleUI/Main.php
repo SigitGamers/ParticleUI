d7790f216426ca4b145de4be6ffda85d22400a5e
SigitGamers <74124100+SigitGamers@users.noreply.github.com>
2022-02-15 13:28:52 +0700

    Update Main.php

--- src/SigitGamersYT/ParticleUI/Main.php
+++ src/SigitGamersYT/ParticleUI/Main.php
@@ -5,10 +5,22 @@ namespace SigitGamersYT\ParticleUI;
 use pocketmine\Server;
 use pocketmine\player\Player;
 
-use pocketmine\utils\Color;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+use pocketmine\task\scheduler\Task;
+
+use pocketmine\plugin\Plugin;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 
+use pocketmine\math\Vector3;
+use pocketmine\world\particle\FlameParticle;
+
 class Main extends PluginBase implements Listener {
+  
+  public function onEnable(): void{
+    $this->getServer()->getPluginManager()->register($this, $this);
+    $this->getLogger()->info("§bEnable!");
+  }
 
 }
