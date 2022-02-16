0dcd2b0970f8a60f515d3c1415c7340dc7a59d23
SigitGamers <74124100+SigitGamers@users.noreply.github.com>
2022-02-16 20:20:58 +0700

    Update Main.php

--- src/SigitGamersYT/ParticleUI/Main.php
+++ src/SigitGamersYT/ParticleUI/Main.php
@@ -5,6 +5,7 @@ namespace SigitGamersYT\ParticleUI;
 use pocketmine\Server;
 use pocketmine\player\Player;
 
+use pocketmine\command\{Command, CommandSender};
 use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat;
 use pocketmine\task\scheduler\Task;
@@ -14,11 +15,28 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 
 use pocketmine\math\Vector3;
-use pocketmine\world\particle\FlameParticle;
+use pocketmine\world\Position;
+use pocketmine\world\particle\HeartParticle; 
+use pocketmine\world\particle\InkParticle;
+use pocketmine\world\particle\LavaParticle;
+use pocketmine\world\particle\AngryVillagerParticle;
+use pocketmine\world\particle\EndermanTeleportParticle;
+use pocketmine\world\particle\CriticalParticle;
+use pocketmine\world\particle\ExplodeParticle;
+use pocketmine\world\particle\HappyVillagerParticle;
+use pocketmine\world\particle\PortalParticle;
+use pocketmine\world\particle\RedstoneParticle;
+use pocketmine\world\particle\SnowballPoofParticle;
+use pocketmine\world\particle\SmokeParticle;
+use pocketmine\world\particle\WaterDripParticle;
 
+use FormAPI\{CustomForm, SimpleForm};
 
 class Main extends PluginBase implements Listener {
   
+  /** @var $players */
+  public $players = [];
+
   public function onEnable(): void{
     $this->getServer()->getPluginManager()->registerEvents($this, $this);
     $this->getServer()->getLogger()->info("§bEnable!");
