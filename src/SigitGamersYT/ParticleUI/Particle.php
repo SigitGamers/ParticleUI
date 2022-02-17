b5a187095fcb7766bd67863db3c2f7aae7adc079
SigitGamers <74124100+SigitGamers@users.noreply.github.com>
2022-02-17 20:57:55 +0700

    Add files via upload

---
+++ src/SigitGamersYT/ParticleUI/Particle.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace SigitGamersYT\ParticleUI;
+
+use pocketmine\Server;
+use pocketmine\player\Player;
+use pocketmine\scheduler\Task;
+
+use pocketmine\math\Vector3;
+use pocketmine\world\Position;
+use pocketmine\world\particle\FlameParticle;
+
+use pocketmine\world\particle\HeartParticle; 
+use pocketmine\world\particle\InkParticle;
+use pocketmine\world\particle\LavaParticle;
+use pocketmine\world\particle\AngryVillagerParticle;
+use pocketmine\world\particle\EndermanTeleportParticle;
+use pocketmine\world\particle\CriticalParticle;
+use pocketmine\world\particle\ExplodeParticle;
+use pocketmine\world\particle\HappyVillagerParticle;
+use pocketmine\world\particle\PortalParticle;
+use pocketmine\world\particle\RedstoneParticle;
+use pocketmine\world\particle\SnowballPoofParticle;
+use pocketmine\world\particle\SmokeParticle;
+use pocketmine\world\particle\WaterDripParticle;
+
+class Particle extends Task{
+
+     public function __construct(Main $plugin){
+        $this->plugin = $plugin;
+     }
+
+     public function onRun(): void{
+          foreach($this->plugin->getServer()->getOnlinePlayers() as $player) {
+		   $name = $player->getName();
+	    	$inv = $player->getInventory();
+			
+		   $players = $player->getWorld()->getPlayers();
+          $level = $player->getWorld();
+
+          $x = $player->getLocation()->getFloorX();
+          $y = $player->getLocation()->getFloorY();
+          $z = $player->getLocation()->getFloorZ();
+          
+          if(in_array($name, $this->plugin->particle1)) {
+				
+		    $center = new Vector3($x, $y, $z);
+	            $particle = new AngryVillagerParticle($center);
+				
+		    $time = 1;
+	            $pi = 3.14159;
+	            $time = $time+0.1/$pi;
+	            for($i = 0; $i <= 2*$pi; $i+=$pi/8){
+		        $x = $time*cos($i) + $player->location->x;
+		        $z = exp(-0.1*$time)*sin($time) + $player->location->z;
+			$y = $time*sin($i) + $player->location->y;
+				
+	                    $particle->setComponents($x, $y, $z);
+		            $level->addParticle($particle);
+				
+		          }
+	      }
+
+         }
+     }
+}
